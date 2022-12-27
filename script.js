61fea890af37f0b09d2b94969498672deb2d2122
OLUWATOMISIN <hardex.tommy@gmail.com>
2022-12-27 10:00:47 +0100

    27-dec

--- script.js
+++ script.js
@@ -18,23 +18,24 @@ function closeNav() {
 
 
 
-
-
 // modal
 
-
 var modal = document.getElementById("myModal");
-
 
 
 
 var img = document.getElementsByClassName("big-shoe");
 var modalImg = document.getElementsByClassName("big-shoee");
 
-img.onclick = function(){
+
+
+ function  press (){
   modal.style.display = "block";
   modalImg.src = this.src;
 }
+
+
+
 
 // var span = document.getElementsByClassName("close")[0];
 
@@ -65,6 +66,7 @@ img.onclick = function(){
 //   document.getElementById("myModal").style.display = "none";
 // }
 
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -80,7 +82,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  // var captionText = document.getElementById("caption");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -99,33 +101,105 @@ function showSlides(n) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlidee(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+//   captionText.innerHTML = dots[slideIndex-1].alt;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // button increment anmd decrement
 
 
-function increment() {
+// function increment() {
 
-var data = Number (document.getElementById("counting").textContent);
-data++
-document.getElementById("counting").textContent = data
-}
-
-
-//creation of decrement function
-function decrement() {
-
-  var data = Number (document.getElementById("counting").textContent);
-  data--
-  document.getElementById("counting").textContent = data
-
-  if (data < 0 ){
-    document.getElementById ("counting").textContent = 0
-  }
-}
+// var data =  (document.getElementById("counting").textContent);
+// data++
+// document.getElementById("counting").textContent = data
+// }
 
 
-function addCart () {
+// //creation of decrement function
+// function decrement() {
 
-  var data = Number ()
+//   var data =  (document.getElementById("counting").textContent);
+//   data--
+//   document.getElementById("counting").textContent = data
 
-}
+//   if (data < 0 ){
+//     document.getElementById ("counting").textContent = 0
+//   }
+// }
+
+
+
+
+
+
+
+document.getElementsByClassName(".plus").addEventListener("click", function () {
+  let data = 0;
+  data = Number(document.getElementById(".counting").textContent);
+  data++;
+
+  document.getElementById(".counting").textContent = data;
+});
+
+
 
